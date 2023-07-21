1953742c57757c5ccd300627306d9ff9a2249d48
Chris Marsh <84872334+ChrisMarsh82@users.noreply.github.com>
2023-07-21 13:13:51 +0100

    upload config

---
+++ config.ts
@@ -0,0 +1,12 @@
+import { extendDeploymentConfig } from "scripts";
+const config = extendDeploymentConfig({ name: "plh_facilitator_zm", parent: "plh_facilitator" });
+
+// override app constants here
+config.app_config.APP_HEADER_DEFAULTS.title = "plh_facilitator_zm";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "plh_facilitator_zm";
+
+// example filters applied to main deployment content
+config.app_data.sheets_filter_function = (flow) =>true,
+config.app_data.assets_filter_function = (fileEntry) =>true
+
+export default config;
