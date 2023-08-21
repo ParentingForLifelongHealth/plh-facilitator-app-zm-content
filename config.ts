dfcb35558d64346ae6c2b5c8661f54ff209bf15a
EsmeeIDEMS <esmee@idems.international>
2023-08-21 15:42:58 +0100

    Change app and sidemenu header to Implementer App

--- config.ts
+++ config.ts
@@ -2,8 +2,8 @@ import { extendDeploymentConfig } from "scripts";
 const config = extendDeploymentConfig({ name: "plh_facilitator_zm", parent: "plh_facilitator" });
 
 // override app constants here
-config.app_config.APP_HEADER_DEFAULTS.title = "plh_facilitator_zm";
-config.app_config.APP_SIDEMENU_DEFAULTS.title = "plh_facilitator_zm";
+config.app_config.APP_HEADER_DEFAULTS.title = "Implementer App";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "Implementer App";
 
 // example filters applied to main deployment content
 config.app_data.sheets_filter_function = (flow) =>true,
