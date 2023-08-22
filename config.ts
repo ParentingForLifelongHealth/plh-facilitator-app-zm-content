bb857812f3a9dd638a4dcd3b043e755615f7c533
Chris Marsh <84872334+ChrisMarsh82@users.noreply.github.com>
2023-08-22 15:37:18 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,12 +1,31 @@
-import { extendDeploymentConfig } from "scripts";
-const config = extendDeploymentConfig({ name: "plh_facilitator_zm", parent: "plh_facilitator" });
+import { generateDeploymentConfig } from "scripts";
+const config = generateDeploymentConfig("plh_facilitator_zm");
 
-// override app constants here
-config.app_config.APP_HEADER_DEFAULTS.title = "plh_facilitator_zm";
-config.app_config.APP_SIDEMENU_DEFAULTS.title = "plh_facilitator_zm";
+/**
+ * The default config should ideally be a superset of any extended configs
+ * to allow for easier post-processing
+ */
 
-// example filters applied to main deployment content
-config.app_data.sheets_filter_function = (flow) =>true,
-config.app_data.assets_filter_function = (fileEntry) =>true
+config.google_drive = {
+  sheets_folder_id: "1k8zJfBeOm18gYDsk0XmdUm7OFtZDUY9R",
+  assets_folder_id: "1KcHDI7O4o2_FZ_YlXsz-8OqN3ehsfdVf",
+};
+
+config.git = {
+  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-zm-content.git",
+  content_tag_latest: "1.1.11",
+};
+
+config.app_data.output_path = "./app_data";
+
+config.app_config.APP_LANGUAGES.default = "gb_en";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "PLH implementers app";
+config.app_config.APP_HEADER_DEFAULTS.title = "PLH implementers app";
+config.app_config.NOTIFICATION_DEFAULTS.title = "New message from PLH implementers app";
+config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from PLH implementers app";
+config.app_config.APP_THEMES.available = ["default", "professional"];
+config.app_config.APP_UPDATES.enabled = true;
+config.app_config.APP_UPDATES.completeUpdateTemplate = "app_update_complete";
+config.app_config.APP_FOOTER_DEFAULTS.templateName = "footer";
 
 export default config;
