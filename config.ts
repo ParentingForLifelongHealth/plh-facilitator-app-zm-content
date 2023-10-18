496106f3690e78ecc43a125a570bc2174b36bbb0
GitHub Action <action@github.com>
2023-10-18 13:59:33 +0000

    Automated commit message

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-zm-content.git",
-  content_tag_latest: "1.1.17",
+  content_tag_latest: "1.1.18",
 };
 
 config.app_data.output_path = "./app_data";
