ff1c88a09399b22e953c8ed33564dc7d9169c44b
cabrinenyona <89448221+cabrinenyona@users.noreply.github.com>
2024-04-03 13:11:10 +0300

    content: 1.1.32

--- config.ts
+++ config.ts
@@ -1,17 +1,17 @@
-import { generateDeploymentConfig } from "scripts";
-const config = generateDeploymentConfig("plh_facilitator_zm");
+import { extendDeploymentConfig } from "scripts";
+const config = extendDeploymentConfig({ name: "plh_facilitator_zm", parent: "plh_facilitator" });
 
-/**
- * The default config should ideally be a superset of any extended configs
- * to allow for easier post-processing
- */
+config.google_drive.sheets_folder_ids =  ["1k8zJfBeOm18gYDsk0XmdUm7OFtZDUY9R", "1bdyRi0SsfuuMuh5vpRr5ixGh6xrBOYEn"];
+config.google_drive.assets_folder_ids = ["1KcHDI7O4o2_FZ_YlXsz-8OqN3ehsfdVf", "1vP339DiDR4R-3q_goOYBl2wpVYP5yBoo"];
 
-config.google_drive = {
-  sheets_folder_ids: ["1k8zJfBeOm18gYDsk0XmdUm7OFtZDUY9R", "1bdyRi0SsfuuMuh5vpRr5ixGh6xrBOYEn"],
-  assets_folder_ids: ["1KcHDI7O4o2_FZ_YlXsz-8OqN3ehsfdVf", "1vP339DiDR4R-3q_goOYBl2wpVYP5yBoo"],
+config.git = {
+  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-zm-content.git",
+  content_tag_latest: "1.1.32",
 };
 
 config.android = {
+  app_id:'international.idems.plh_facilitator_zm',
+  app_name:'HHPP Yanga',
   splash_asset_path: "./app_data/assets/android/splash.png",
   icon_asset_path: "./app_data/assets/android/icon.png",
   icon_asset_foreground_path: "./app_data/assets/android/icon-foreground.png",
@@ -19,13 +19,9 @@ config.android = {
 };
 
 config.web.favicon_asset = "images/logos/hhpp_yanga_logos_icon_colour.svg";
-
-config.git = {
-  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-zm-content.git",
-  content_tag_latest: "1.1.30",
-};
-
 config.api.db_name = "plh_facilitator_zm"
+
+// Hacky fix to point extended deployment to content within its own repo
 config.app_data.output_path = "./app_data";
 
 config.app_config.APP_LANGUAGES.default = "gb_en";
@@ -33,11 +29,8 @@ config.app_config.APP_SIDEMENU_DEFAULTS.title = "HHPP Yanga";
 config.app_config.APP_HEADER_DEFAULTS.title = "HHPP Yanga";
 config.app_config.NOTIFICATION_DEFAULTS.title = "New message from HHPP Yanga";
 config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from HHPP Yanga";
-config.app_config.APP_THEMES.available = ["default", "professional"];
-config.app_config.APP_THEMES.defaultThemeName = "professional";
-config.app_config.APP_UPDATES.enabled = true;
-config.app_config.APP_UPDATES.completeUpdateTemplate = "app_update_complete";
-config.app_config.APP_FOOTER_DEFAULTS.templateName = "footer";
+
 config.error_logging = { dsn: "https://cd3bdbb9bb46433ca6969c7a775206ac@app.glitchtip.com/5661"};
 
 export default config;
+
